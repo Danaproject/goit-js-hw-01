c4f2a0d8faa56ce51439d42bb11357253264bb60
Dana Project <70840490+Danaproject@users.noreply.github.com>
2020-11-25 23:08:40 +0100

    +second solution

---
+++ js/task-04-1.js
@@ -0,0 +1,17 @@
+let credits = 23580;
+const pricePerDroid = 3000;
+const numberInputRef = document.querySelector('input[name="number"]');
+const buttonRef = document.querySelector('button[class="buy"]');
+
+buttonRef.addEventListener('click', function () {
+    console.log('количество дроидов:', numberInputRef.value);
+    const numberOfDoid = Number(numberInputRef.value);
+    const totalPrice = numberOfDoid * pricePerDroid;
+
+    if (totalPrice > credits) {
+        console.log('Недостаточно средств на счету!');
+    } else {
+        credits -= totalPrice;
+        console.log(`Вы купили ${numberOfDoid} дроидов, на счету осталось ${credits} кредитов.`);
+    }
+});
---
+++ js/task-05-1.js
@@ -0,0 +1,34 @@
+let price;
+let notAvailable;
+
+const countryInputRef = document.querySelector('input[name="country"]');
+const buttonRef = document.querySelector('button[class="send"]');
+
+buttonRef.addEventListener('click', function () {
+    let country = String(countryInputRef.value);
+    country = country.charAt(0).toUpperCase() + country.slice(1).toLowerCase();
+   
+    switch (country) {
+        case 'Китай':
+            price = 100;
+            break;
+        case 'Чили':
+            price = 250;
+            break;
+        case 'Австралия':
+            price = 170;
+            break;
+        case 'Индия':
+            price = 80;
+            break;
+        case 'Ямайка':
+            price = 120;
+            break;
+        default:
+            alert('В вашей стране доставка не доступна');
+            notAvailable = 1;
+    }
+    if (notAvailable !== 1) {
+        console.log(`Доставка в ${country} будет стоить ${price} кредитов`);
+    }
+});
